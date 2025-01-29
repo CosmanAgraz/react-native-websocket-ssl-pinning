640d681d4502be35c28fe541ec576051760c5b57
scosmanagraz <scosmanagraz@carehawk.com>
2025-01-29 17:06:42 -0500

    Added ios stuff

--- ios/WebSocketSslPinning/WebSocketSslPinning.h
+++ ios/WebSocketSslPinning/WebSocketSslPinning.h
@@ -1,12 +1,46 @@
-#import <Foundation/Foundation.h>
+//
+//  RCTWebSocketSsLPinning.h
+//  VCallPlus
+//
+//  Created by Akhil Thomas on 2024-12-31.
+//
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+#ifndef RCTWebSocketSsLPinning_h
+#define RCTWebSocketSsLPinning_h
+
+//  RCTCalendarModule.h
 #import <React/RCTBridgeModule.h>
-#endif
+#import <React/RCTEventEmitter.h>
 
-@interface WebSocketSsLPinning : NSObject <RCTBridgeModule>
+#import "WebSocketStates.h"
 
+@interface RCTWebSocketSslPinning : RCTEventEmitter <RCTBridgeModule, NSURLSessionWebSocketDelegate>
+
+@property bool connectionDrop;
+@property bool hasListeners;
+
+- (void)startObserving;
+- (void)stopObserving;
+
+// Socket
+@property (nonatomic, strong) NSURLSession *session;
+@property (nonatomic, strong) NSURLSessionWebSocketTask *webSocketTask;
+@property WebSocketState *state;
+
+- (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask
+    didOpenWithProtocol:(NSString *)protocol;
+
+- (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didCloseWithCode:(NSURLSessionWebSocketCloseCode)closSeCode reason:(NSData *)reason;
+
+- (void)startReceivingMessages;
+
+- (void)URLSession:(NSURLSession *)session
+              task:(NSURLSessionTask *)task
+didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler;
+
+// React Native
+-(void)sendEventToJavaScript:(NSString *)eventName withParams:(id)params;
 @end
-  
+
+#endif /* RCTWebSocketSsLPinning_h */
--- ios/WebSocketSslPinning/WebSocketSslPinning.m
+++ ios/WebSocketSslPinning/WebSocketSslPinning.m
@@ -1,290 +1,151 @@
 //
-//  Created by Max Toyberman on 13/10/16.
+//  RCTWebSocketSsLPinning.m
+//  VCallPlus
+//
+//  Created by Akhil Thomas on 2024-12-31.
+//
+// IMPORTANT: make sure to invoke the callback function on the RCT_EXPORT_METHODs
+// failing to invoke the callback will result in memory leaks and may crash the app.
 
-#import "WebSocketSsLPinning.h"
-#import "AFNetworking.h"
+#import <React/RCTLog.h>
+#import "RCTWebSocketSslPinning.h"
 
-@interface WebSocketSsLPinning()
+@implementation RCTWebSocketSslPinning
 
-@property (nonatomic, strong) NSURLSessionConfiguration *sessionConfig;
-
-@end
-
-@implementation WebSocketSsLPinning
 RCT_EXPORT_MODULE();
 
-- (instancetype)init
+RCT_EXPORT_METHOD(fetch:(NSString *)urlString withOptions:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
 {
-    self = [super init];
-    if (self) {
-        self.sessionConfig = [NSURLSessionConfiguration ephemeralSessionConfiguration];
-        self.sessionConfig.HTTPCookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    }
-    return self;
+  RCTLogInfo(@"iOS: Connecting to WebSocket at %@ with options: %@", urlString, options.description);
+    
+  _session = [NSURLSession sessionWithConfiguration:NSURLSessionConfiguration.defaultSessionConfiguration
+                                           delegate:self
+                                      delegateQueue:[NSOperationQueue mainQueue]];
+  
+  _webSocketTask = [_session webSocketTaskWithURL:[NSURL URLWithString:urlString]];
+  
+  [_webSocketTask resume];
+    
+  callback(@[[NSNull null], @"WebSocket fetch initiated."]);
 }
 
-RCT_EXPORT_METHOD(getCookies: (NSURL *)url resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
-    
-    NSHTTPCookie *cookie;
-    NSHTTPCookieStorage* cookieJar  =  NSHTTPCookieStorage.sharedHTTPCookieStorage;
-    
-    NSMutableDictionary* dictionary = @{}.mutableCopy;
-    
-    for (cookie in [cookieJar cookiesForURL:url]) {
-        [dictionary setObject:cookie.value forKey:cookie.name];
-    }
-    
-    if ([dictionary count] > 0){
-        resolve(dictionary);
-    }
-    else{
-        NSError *error = nil;
-        reject(@"no_cookies", @"There were no cookies", error);
-    }
-}
+RCT_EXPORT_METHOD(sendWebSocketMessage:(NSString *)payload callback:(RCTResponseSenderBlock)callback)
+{
+  if (_webSocketTask.state == NSURLSessionTaskStateRunning) {
+    NSURLSessionWebSocketMessage *message = [[NSURLSessionWebSocketMessage alloc] initWithString:payload];
 
-
-
-RCT_EXPORT_METHOD(removeCookieByName: (NSString *)cookieName
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject) {
-    
-    NSHTTPCookieStorage *cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    for (NSHTTPCookie *cookie in cookieStorage.cookies) {
-        // [cookieStorage deleteCookie:each];
-        NSString * name = cookie.name;
-        
-        if([cookieName isEqualToString:name]) {
-            [cookieStorage deleteCookie:cookie];
-        }
-    }
-    
-    resolve(nil);
-    
-}
-
-
--(void)performRequest:(AFURLSessionManager*)manager  obj:(NSDictionary *)obj  request:(NSMutableURLRequest*) request callback:(RCTResponseSenderBlock) callback  {
-    
-    [[manager dataTaskWithRequest:request uploadProgress:nil downloadProgress:nil completionHandler:^(NSURLResponse * _Nonnull response, id _Nullable responseObject, NSError * _Nullable error) {
-        
-        
-        NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*) response;
-        NSString *bodyString = [[NSString alloc] initWithData: responseObject encoding:NSUTF8StringEncoding];
-        NSInteger statusCode = httpResp.statusCode;
-        
-        if (!error) {
-            // if(obj[@"responseType"]){
-            NSString * responseType = obj[@"responseType"];
-            
-            if ([responseType isEqualToString:@"base64"]){
-                NSString* base64String = [responseObject base64EncodedStringWithOptions:0];
-                callback(@[[NSNull null], @{
-                               @"status": @(statusCode),
-                               @"headers": httpResp.allHeaderFields,
-                               @"data": base64String
-                }]);
-            }
-            else {
-                callback(@[[NSNull null], @{
-                               @"status": @(statusCode),
-                               @"headers": httpResp.allHeaderFields,
-                               @"bodyString": bodyString ? bodyString : @""
-                }]);
-            }
-        } else if (error && error.userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey]) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                callback(@[@{
-                               @"status": @(statusCode),
-                               @"headers": httpResp.allHeaderFields,
-                               @"bodyString": bodyString ? bodyString : @""
-                }, [NSNull null]]);
-            });
-        } else {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                callback(@[error.localizedDescription, [NSNull null]]);
-            });
-        }
-    }] resume];
-    
-}
-
-
--(void) setHeaders: (NSDictionary *)obj request:(NSMutableURLRequest*) request {
-    
-    if (obj[@"headers"] && [obj[@"headers"] isKindOfClass:[NSDictionary class]]) {
-        NSMutableDictionary *m = [obj[@"headers"] mutableCopy];
-        for (NSString *key in [m allKeys]) {
-            if (![m[key] isKindOfClass:[NSString class]]) {
-                m[key] = [m[key] stringValue];
-            }
-        }
-        [request setAllHTTPHeaderFields:m];
-    }
-    
-}
-
-- (BOOL) isFilePart: (NSArray*)part {
-    if (![part[1] isKindOfClass:[NSDictionary class]]) {
-        return NO;
-    }
-    NSDictionary * value = part[1];
-    return [value objectForKey:@"type"] && ([value objectForKey:@"name"] || [value objectForKey:@"fileName"]);
-}
-
--(void) appendFormDataFilePart: (id<AFMultipartFormData>) formData fileData: (NSArray*) fileData  {
-    NSString * key = fileData[0];
-    NSDictionary * value = fileData[1];
-    NSString * fileName = [value objectForKey:@"name"] ? [value objectForKey:@"name"] : [value objectForKey:@"fileName"];
-    NSString * mimeType = [value objectForKey:@"type"];
-    NSString * path = [value objectForKey:@"uri"] ? [value objectForKey:@"uri"] : [value objectForKey:@"path"];
-    
-    [formData appendPartWithFileURL:[NSURL URLWithString:path] name:key fileName:fileName mimeType:mimeType error:nil];
-}
-
--(void) performMultipartRequest: (AFURLSessionManager*)manager obj:(NSDictionary *)obj url:(NSString *)url request:(NSMutableURLRequest*) request callback:(RCTResponseSenderBlock) callback formData:(NSDictionary*) formData {
-    NSString * method = obj[@"method"] ? obj[@"method"] : @"POST";
-    
-    NSMutableURLRequest *formDataRequest = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:method URLString:url parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> _formData) {
-        if([formData objectForKey:@"_parts"]){
-            NSArray * parts = formData[@"_parts"];
-            for (int i = 0; i < [parts count]; i++)
-            {
-                NSArray * part = parts[i];
-                NSString * key = part[0];
-                
-                if ([self isFilePart:part]) {
-                    [self appendFormDataFilePart:_formData fileData: part];
-                } else {
-                    NSString * value = part[1];
-                    NSData *data = [value dataUsingEncoding:NSUTF8StringEncoding];
-                    [_formData appendPartWithFormData:data name: key];
-                }
-            }
-        }
-    } error:nil];
-    
-    // Migrate header fields.
-    [formDataRequest setAllHTTPHeaderFields:[request allHTTPHeaderFields]];
-    
-    NSURLSessionUploadTask *uploadTask = [manager
-                                          uploadTaskWithStreamedRequest:formDataRequest
-                                          progress:^(NSProgress * _Nonnull uploadProgress) {
-        NSLog(@"Upload progress %lld", uploadProgress.completedUnitCount / uploadProgress.totalUnitCount);
-    }
-                                          completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
-        NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*) response;
-        NSString *bodyString = [[NSString alloc] initWithData: responseObject encoding:NSUTF8StringEncoding];
-        NSInteger statusCode = httpResp.statusCode;
-        if (!error) {
-            
-            NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*) response;
-            
-            NSString *bodyString = [[NSString alloc] initWithData: responseObject encoding:NSUTF8StringEncoding];
-            NSInteger statusCode = httpResp.statusCode;
-            
-            NSDictionary *res = @{
-                @"status": @(statusCode),
-                @"headers": httpResp.allHeaderFields,
-                @"bodyString": bodyString ? bodyString : @""
-            };
-            callback(@[[NSNull null], res]);
-        }
-        else if (error && error.userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey]) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                callback(@[@{
-                               @"status": @(statusCode),
-                               @"headers": httpResp.allHeaderFields,
-                               @"bodyString": bodyString ? bodyString : @""
-                }, [NSNull null]]);
-            });
-        } else {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                callback(@[error.localizedDescription, [NSNull null]]);
-            });
-        }
+    // Send the message
+    [_webSocketTask sendMessage:message completionHandler:^(NSError * _Nullable error) {
+      if (error) {
+        callback(@[[@"Failed to send message: " stringByAppendingString:error.localizedDescription] , [NSNull null]]);
+      }
     }];
     
-    [uploadTask resume];
+    //send message
+  callback(@[[NSNull null], @"Message sent successfully."]);
+  } else {
+      RCTLogError(@"iOS: Failed to send message: WebSocket is not connected.");
+      callback(@[@"Failed to send message.", [NSNull null]]);
+  }
 }
 
-RCT_EXPORT_METHOD(fetch:(NSString *)url obj:(NSDictionary *)obj callback:(RCTResponseSenderBlock)callback) {
-    NSURL *u = [NSURL URLWithString:url];
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:u];
-    
-    AFSecurityPolicy *policy;
-    BOOL pkPinning = [[obj objectForKey:@"pkPinning"] boolValue];
-    BOOL disableAllSecurity = [[obj objectForKey:@"disableAllSecurity"] boolValue];
-    
-    NSSet *certificates = [AFSecurityPolicy certificatesInBundle:[NSBundle mainBundle]];
-    
-    // set policy (ssl pinning)
-    if(disableAllSecurity){
-        policy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
-        policy.validatesDomainName = false;
-        policy.allowInvalidCertificates = true;
-    }
-    else if (pkPinning){
-        policy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModePublicKey withPinnedCertificates:certificates];
-    }
-    else{
-        policy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeCertificate withPinnedCertificates:certificates];
-    }
-    
-    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-    manager.securityPolicy = policy;
-    
-    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-    
-    
-    if (obj[@"method"]) {
-        [request setHTTPMethod:obj[@"method"]];
-    }
-    if (obj[@"timeoutInterval"]) {
-        [request setTimeoutInterval:[obj[@"timeoutInterval"] doubleValue] / 1000];
-    }
-    
-    if(obj[@"headers"]) {
-        [self setHeaders:obj request:request];
-    }
-    
-    if (obj) {
-        
-        if ([obj objectForKey:@"body"]) {
-            NSDictionary * body = obj[@"body"];
-            
-            // this is a multipart form data request
-            if([body isKindOfClass:[NSDictionary class]]){
-                // post multipart
-                if ([body objectForKey:@"formData"]) {
-                    [self performMultipartRequest:manager obj:obj url:url request:request callback:callback formData:body[@"formData"]];
-                } else if ([body objectForKey:@"_parts"]) {
-                    [self performMultipartRequest:manager obj:obj url:url request:request callback:callback formData:body];
-                }
-            }
-            else {
-                
-                // post a string
-                NSData *data = [obj[@"body"] dataUsingEncoding:NSUTF8StringEncoding];
-                [request setHTTPBody:data];
-                [self performRequest:manager obj:obj request:request callback:callback ];
-                //TODO: if no body
-            }
-            
-        }
-        else {
-            [self performRequest:manager obj:obj request:request callback:callback ];
-        }
-    }
-    else {
-        
-    }
-    
-}
-
-+ (BOOL)requiresMainQueueSetup
+RCT_EXPORT_METHOD(terminateWebSocket:(NSString *)reason callback:(RCTResponseSenderBlock)callback)
 {
-    return YES;
+  [_webSocketTask cancel];
+  callback(@[[NSNull null], @"WebSocket connection terminated."]);
 }
+
+RCT_EXPORT_METHOD(closeWebSocket:(NSString *)reason callback:(RCTResponseSenderBlock)callback)
+{
+    if (_webSocketTask.state == NSURLSessionTaskStateRunning) {
+        // close connection with reason
+      [_webSocketTask cancel];
+      callback(@[[NSNull null], @"WebSocket connection closed."]);
+    } else {
+      callback(@[@"WebSocket is not connected.", [NSNull null]]);
+    }
+}
+
+/* Events we are allowed to send the Javascript execution context.
+ */
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"onOpen", @"onClosed", @"onMessage", @"onFailure"];
+}
+
+/* Sends information to Javascript execution context.
+ */
+- (void)sendEventToJavaScript:(NSString *)eventName withParams:(id)params {
+    if (self.bridge && _hasListeners) {
+        [self sendEventWithName:eventName body:params];
+    }
+}
+
+- (void)startObserving {
+    _hasListeners = YES;
+}
+
+- (void)stopObserving {
+    _hasListeners = NO;
+}
+
+// Web Socket stuff
+- (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didOpenWithProtocol:(nullable NSString *) protocol {
+  NSLog(@"iOS: Web Socket is open");
+  [self startReceivingMessages];
+  [self sendEventToJavaScript:@"onOpen" withParams:@{@"code": @"101", @"message": @"ok"}];
+};
+
+- (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didCloseWithCode:(NSURLSessionWebSocketCloseCode)closeCode reason:(nullable NSData *)reason{
+  NSLog(@"iOS: Web socket is closed");
+  [self sendEventToJavaScript:@"onClosed" withParams:@{@"code": @"101", @"message": @"ok"}];
+};
+
+- (void)startReceivingMessages {
+  [_webSocketTask receiveMessageWithCompletionHandler:^(NSURLSessionWebSocketMessage * _Nullable message, NSError * _Nullable error) {
+    if (error) {
+      NSLog(@"iOS: Error receiving message: %@", error.localizedDescription);
+      return;
+    }
+    
+    if (message.type == NSURLSessionWebSocketMessageTypeString) {
+      [self sendEventToJavaScript:@"onMessage" withParams:@{@"payload": message.string}];
+    } else if (message.type == NSURLSessionWebSocketMessageTypeData) {
+      NSLog(@"iOS: Received binary message: %@", message.data);
+    } else {
+      NSLog(@"iOS: Received unknown message type.");
+    }
+    
+    [self startReceivingMessages];
+  }];
+};
+
+- (void)URLSession:(NSURLSession *)session
+              task:(NSURLSessionTask *)task
+didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler {
+  // NSLog(@"iOS: Challenge Protection Space: %@ (%@)", challenge.protectionSpace.host, challenge.protectionSpace.authenticationMethod);
+  // NSLog(@"iOS: Challenge Previous Failure Count: %ld", (long)challenge.previousFailureCount);
+
+  // Check the authentication method
+  if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+      SecTrustRef serverTrust = challenge.protectionSpace.serverTrust;
+      if (serverTrust) {
+          NSURLCredential *credential = [NSURLCredential credentialForTrust:serverTrust];
+          // NSLog(@"iOS: Server trust credential created: %@", credential);
+          
+          completionHandler(NSURLSessionAuthChallengeUseCredential, credential);
+          // NSLog(@"iOS: CompletionHandler called with disposition: UseCredential.");
+      } else {
+          NSLog(@"iOS: Server trust is nil. Cancelling authentication challenge.");
+          completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, nil);
+      }
+  } else {
+      NSLog(@"iOS: Challenge authentication method is unsupported: %@", challenge.protectionSpace.authenticationMethod);
+      
+      // Default handling for unsupported challenges
+      completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+      NSLog(@"iOS: CompletionHandler called with disposition: PerformDefaultHandling.");
+  }
+};
 
 @end
